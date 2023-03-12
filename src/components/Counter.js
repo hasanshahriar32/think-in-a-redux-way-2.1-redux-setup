135080d1622f52c1802b41dade28d586a663a4f1
Mr. Hasan <hasanshahriar32@gmail.com>
2023-03-12 18:04:52 +0600

    counter function completed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,51 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
+import { connect } from "react-redux";
+import { decrement, increment } from "../redux/counter/actions";
 
-    const increment = () => {
-        setCount((prevCount) => prevCount + 1);
-    };
+function Counter({ decrement, increment, count }) {
+  // const [count, setCount] = useState(0);
 
-    const decrement = () => {
-        setCount((prevCount) => prevCount - 1);
-    };
+  // const increment = () => {
+  //   setCount((prevCount) => prevCount + 1);
+  // };
 
-    return (
-        <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-            <div className="text-2xl font-semibold">{count}</div>
-            <div className="flex space-x-3">
-                <button
-                    className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    onClick={increment}
-                >
-                    Increment
-                </button>
-                <button
-                    className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    onClick={decrement}
-                >
-                    Decrement
-                </button>
-            </div>
-        </div>
-    );
+  // const decrement = () => {
+  //   setCount((prevCount) => prevCount - 1);
+  // };
+
+  return (
+    <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+      <div className="text-2xl font-semibold">{count}</div>
+      <div className="flex space-x-3">
+        <button
+          className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+          onClick={increment}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-red-400 text-white px-3 py-2 rounded shadow"
+          onClick={decrement}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.value,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: (value) => dispatch(increment(value)),
+    decrement: (value) => dispatch(decrement(value)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
